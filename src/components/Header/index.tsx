336874125d3ab6e9c7bc06b967ca6959d5e17d35
Pedro de Araújo <pedromsra@gmail.com>
2023-11-09 02:27:54 -0300

    removing ununsed import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FunctionComponent, SetStateAction, useEffect, useRef, useState } from "react";
-import { useDispatch, ProviderProps } from "react-redux";
+import { useDispatch } from "react-redux";
 
 // import { useAuth } from "../../hooks/auth";
 
